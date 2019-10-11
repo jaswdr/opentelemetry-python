ddf09786d074b0bf902220baffd19a1eeab900e2
Johannes Liebermann <johannes@kinvolk.io>
2019-10-11 15:56:31 +0200

    Allow passing a timestamp to add_event()

--- opentelemetry-sdk/tests/trace/test_trace.py
+++ opentelemetry-sdk/tests/trace/test_trace.py
@@ -174,8 +174,10 @@ class TestSpan(unittest.TestCase):
 
             # events
             root.add_event("event0")
-            root.add_event("event1", {"name": "birthday"})
             now = time_ns()
+            root.add_event(
+                "event1", timestamp=now, attributes={"name": "birthday"}
+            )
             root.add_lazy_event(
                 trace_api.Event("event2", now, {"name": "hello"})
             )
@@ -187,6 +189,7 @@ class TestSpan(unittest.TestCase):
 
             self.assertEqual(root.events[1].name, "event1")
             self.assertEqual(root.events[1].attributes, {"name": "birthday"})
+            self.assertEqual(root.events[1].timestamp, now)
 
             self.assertEqual(root.events[2].name, "event2")
             self.assertEqual(root.events[2].attributes, {"name": "hello"})
