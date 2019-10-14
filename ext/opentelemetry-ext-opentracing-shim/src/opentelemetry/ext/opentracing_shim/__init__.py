83d5a7523d5a3b6a90ed64c9f28b781783e27577
Johannes Liebermann <johannes@kinvolk.io>
2019-10-14 21:05:24 +0200

    Remove unnecessary comments.
    These are obvious and therefore unnecessary.

--- ext/opentelemetry-ext-opentracing-shim/src/opentelemetry/ext/opentracing_shim/__init__.py
+++ ext/opentelemetry-ext-opentracing-shim/src/opentelemetry/ext/opentracing_shim/__init__.py
@@ -56,7 +56,6 @@ class SpanWrapper(opentracing.Span):
     def set_operation_name(self, operation_name):
         self._otel_span.update_name(operation_name)
 
-        # Return self for call chaining.
         return self
 
     def finish(self, finish_time=None):
@@ -68,7 +67,6 @@ class SpanWrapper(opentracing.Span):
     def set_tag(self, key, value):
         self._otel_span.set_attribute(key, value)
 
-        # Return self for call chaining.
         return self
 
     def log_kv(self, key_values, timestamp=None):
@@ -80,7 +78,6 @@ class SpanWrapper(opentracing.Span):
         event_name = util.event_name_from_kv(key_values)
         self._otel_span.add_event(event_name, event_timestamp, key_values)
 
-        # Return self for call chaining.
         return self
 
     def set_baggage_item(self, key, value):
